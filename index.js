fc5819f9daf176a3b31178f16adf8356b94d424f
Facundo_Loto <55114127+facundoloto@users.noreply.github.com>
2021-08-20 13:00:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,15 +21,15 @@ app.use('/static', express.static('public')); //esto sirve para que el cliente p
 
 app.use(cookieParser());
 app.use(express.json())
-//app.use(cors())
+app.use(cors())
 // Configurar cabeceras y cors
-/*app.use((req, res, next) => {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-});*/
+});
 
 app.post("/login/signup",descargar.upload,login.createUsuario)
 app.post("/login/search",login.searchUsuario)
