84e5c5ced21976b2cac62dbeca2bab5354acf27a
Facundo_Loto <55114127+facundoloto@users.noreply.github.com>
2021-08-20 13:16:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ app.use(cookieParser());
 app.use(express.json())
 app.use(cors())
 // Configurar cabeceras y cors
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-});
+});*/
 
 app.post("/login/signup",descargar.upload,login.createUsuario)
 app.post("/login/search",login.searchUsuario)
